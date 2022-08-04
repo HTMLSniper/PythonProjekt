bdfd41c8c44674db447250b379f13d7a6cea1c12
Colin Vavra <c.vavra@web.de>
2022-08-04 22:44:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import time
 
 
-class App(tk.Tk):
+class Gui(tk.Tk):
     def __init__(self):
         super().__init__()
         self.style = ttk.Style(self)
@@ -42,46 +42,39 @@ class App(tk.Tk):
         self.checkCanBuy()
         self.after(1000,lambda: self.updateNumbers())
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+class UpgradeButton():
+    def __init__(self, root, text):
+        self.root = root
+        self.index = 0
+        self.perSec = perSec
+        self.cost = cost
+        
+        self.text = createText()
+        self.command = self.pressed
+        self.state = "disabled"
+        self.button = ttk.Button(self.root, text = self.text, command = self.command, state = self.state)
+    
+    def createText(self):
+        text = str(self.perSec) + " für " + str(self.cost)
+        return text
+    
+    def updateButton(self):
+        self.button.config(self.root, text = self.text, state = self.state)
+    
+    def checkDisabled(self, index):
+        if index >= self.cost:
+            self.state = "normal"
+            updateButton()
+    
+    def upgradeIndex(self):
+    
+    def pressed(self):
+        self.root.upgradeBought(self.cost)
+        self.index =+ 1
+        self.state = "disabled"
+        
+            
 
-# from tkinter import *
-# 
-# class Gui(Tk):
-#     def __init__(self):
-#         super.__init__()
-#         self.index = 0
-#         self.perSec = 0
-#         self.root = Tk()
-#         self.indexLb = Label(self.root, text = str(self.index), width = 20)
-#         self.perSecLb = Label(self.root, text = str(self.perSec), width = 20)
-#         self.mainButton = Button(self.root, text = "Press", command = self.mainButtonPressed)
-#         self.upgradeButton = Button(self.root, text = "+1/s", command = self.upgradeButtonPressed)
-#         
-#         
-#         self.indexLb.grid(row = 0, column = 0, columnspan = 1, pady = 5)
-#         self.perSecLb.grid(row = 1, column = 0, columnspan = 1, pady = 5)
-#         self.mainButton.grid(row = 2, column = 0, pady = 5)
-#         self.upgradeButton.grid(row = 0, column = 1, pady = 5)
-#         
-#         self.updateNumbers()
-#         self.root.mainloop()
-#     
-#     def mainButtonPressed(self):
-#         self.index += 1
-#         self.indexLb.config(text = str(self.index))
-#         
-#     def upgradeButtonPressed(self):
-#         self.perSec += 1
-#         self.perSecLb.config(text = str(self.perSec))
-#         
-#         
-#     def updateNumbers(self):
-#         self.index += self.perSec
-#         self.indexLb.config(text = str(self.index))
-#         self.perSecLb.config(text = str(self.perSec))
-#         self.after(1000, self.updateNumbers)
-#         
-#         
-# test = Gui()
+if __name__ == "__main__":
+    gui = Gui()
+    gui.mainloop()
